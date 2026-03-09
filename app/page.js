0926f3cf961cf136608545b5638b4ae790a330b0
Gurjot Hundal <gurjothundal@Gurjots-MacBook-Air-2.local>
2026-03-08 17:27:22 -0700

    add html website element base

--- app/page.js
+++ app/page.js
@@ -4,63 +4,301 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+
+          {/* header section fr nav bar, logo, website header, and BC flag */}
+
+
+          <div class="header-section">
+            <div class="header-content">
+
+
+
+
+              <div class="bcFlag"></div>
+
+
+
+              <div class="hydro-ed-banner">
+
+                <div class="hydro-ed-logo"></div>
+
+                <div class="hydro-ed-title"> <h1>HYDRO-ED</h1></div>
+
+              </div>
+
+
+
+              <nav class="nav-bar">
+
+                <a href="#game">Game</a>
+                <a href="#instructions"></a>
+                <a href="#education"></a>
+                <a href="#about-us">About Us</a>
+
+              </nav>
+
+            </div>
+
+
+
+          </div>
+
+          {/* header section fr nav bar, logo, website header, and BC flag */}
+
+
+
+
+
+
+
+
+          {/* section for the interactive game  */}
+
+          <div class="Game">
+
+            <div class="Game-header-section">
+
+              <div id="game-header"> <h2>GAME</h2>
+              
+              </div>
+
+            </div>
+
+
+
+
+            <div class="Game-interactive-section">
+
+              <div class="Game-content">
+
+                <button class="turn-on-water">
+
+                  <h2>TURN ON WATER</h2>
+
+                </button>
+
+              </div>
+
+
+            </div>
+
+          </div>
+
+          {/* section for the interactive game  */}
+
+
+
+
+
+
+
+
+
+          {/* instructions section  */}
+
+          <div class="instructions-section">
+            
+            <div class="instructions-header">
+
+              <div id="instructions-title">
+
+                <h2>INSTRUCTIONS</h2>
+
+              </div>
+
+            </div>
+            
+
+            <div class="instructions-content">
+
+              <div class="instructions-text">
+
+                <p>Press the green button to turn on the water.</p>
+
+                <p>As water wheel begins to turn the generator to generate energy, Press the generator.</p>
+
+                <p>As energy generates from the turning of the waterwheel, Press the transformer to convert the energy to volts.</p>
+
+                <p>As volts reaches the meter, Press the meter to track and distribute the electricity to consumers from the power grid.</p>
+
+              </div>
+
+
+              <div class="instructions-image">
+
+                <img></img>
+
+              </div>
+
+            </div>
+
+
+
+          </div>
+          
+          {/* instructions section  */}
+
+
+
+
+
+          {/* education section  */}
+
+          <div class="education-section">
+
+
+
+            <div class="education-header">
+
+              <h2>EDUCATION</h2>
+            </div>
+
+
+
+            <div class="education-visuals">
+
+              <div class="bc-hydro-logo"></div>
+
+
+              <div class="education-images">
+
+                <img></img>
+                <img></img>
+                <img></img>
+
+              </div>
+
+
+            </div>
+
+
+            <div class="education-content">
+
+              <div class="fun-facts">
+
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+
+
+              </div>
+
+
+            </div>
+
+
+
+          </div>
+
+          {/* education section  */}
+
+
+
+
+
+
+
+
+
+
+
+
+            {/* about us section */}
+
+          <div class="about-us">
+
+            <div class="about-header">
+
+              <div id="about-us-title"> 
+                
+                <h1>ABOUT US</h1>
+              
+              </div>
+
+
+            </div>
+
+
+            <div class="about-us-content">
+
+
+
+              <div class="about-us-bio">
+
+                <div class="bio-images">
+
+                  <img id="gurjot"></img>
+
+                  <img id="david"></img>
+
+                </div>
+
+
+                <div class="who-we-are">
+
+                  <div id="gurjot-bio">
+
+                    <p></p>
+                  </div>
+
+                  <div id="david-bio">
+
+                    <p></p>
+
+                  </div>
+
+
+                </div>
+
+              </div>
+
+
+
+
+
+              <div class="additional-info">
+
+
+
+                <div class="purpose">
+
+                  <p></p>
+                </div>
+
+
+
+                <div class="ambitions">
+
+                  <p></p>
+
+                </div>
+
+
+              </div>
+
+
+
+
+
+            </div>
+
+
+
+          </div>
+
+          {/* about us section */}
+
+
+
+
+
+
+
+
+     
     </div>
   );
 }
