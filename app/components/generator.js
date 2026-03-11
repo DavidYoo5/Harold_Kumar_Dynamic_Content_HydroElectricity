ecff2a5e5d7de5c5e6f10d0cf7bf45e1ba7db37b
Gurjot Hundal <gurjothundal@Gurjots-MacBook-Air-2.local>
2026-03-11 16:12:19 -0700

    set up generator file

---
+++ app/components/generator.js
@@ -0,0 +1,22 @@
+"use client";
+
+import Image from "next/image";
+
+
+export default function Generator({ generatorOn}) {
+    return (
+        <div className="generator">
+            <Image src="/images/generator.png" alt="Generator" width={300} height={300} />
+        </div>
+
+
+
+
+
+
+
+    );
+}
+
+
+
